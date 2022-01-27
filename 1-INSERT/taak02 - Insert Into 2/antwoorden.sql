dd21e02141a15ac777d73c1775b6e4f4256763c7
Thomas Mooij <2021747@talnet.nl>
2022-01-27 17:28:40 +0100

    advancedSQL taak02 AF

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -2,7 +2,7 @@ INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, w
 VALUES (8, 'Gert' , 'Gertzoon' , '13', '1998-03-08', 'Uithoorn')
 
 INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats)
-VALUES (9, 'Bert' , 'Hendriks' , '13', '1994-04-25', 'Uithoorn')
+VALUES (9, 'Janus' , 'Pipelanus' , '13', '1994-04-25', 'Uithoorn')
 
 INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats) 
-VALUES (10, 'Marri' , 'Janssen' , '13', '1998-06-26', 'Uithoorn')
+VALUES (10, 'Esmeralda' , 'Alvarez' , '13', '1998-06-26', 'Uithoorn')
