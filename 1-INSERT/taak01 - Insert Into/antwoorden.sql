d6d0dd3d9f23a2274889cdd477faea98b0642a3e
Thomas Mooij <2021747@talnet.nl>
2022-01-31 17:58:09 +0100

    taak01 af

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,6 @@
 
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (1, 'Gert', 'Gertus', '13A', 'Mijnstraat11', '1234AB', '1910-06-18', 'Uithoorn');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (2, 'Piet', 'Pieters', '13A', 'straatstraat13', '1234AB', '1910-04-07', 'Uithoorn');
