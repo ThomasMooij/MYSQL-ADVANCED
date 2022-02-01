cda3473b47d792b7d8ce4bed67942140bf506327
Thomas Mooij <2021747@talnet.nl>
2022-02-01 13:26:21 +0100

    insert into

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,8 +1,8 @@
-INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats) 
-VALUES (8, 'Gert' , 'Gertzoon' , '13', '1998-03-08', 'Uithoorn')
+INSERT INTO studenten ( voornaam , achternaam, geboortedatum,woonplaats ) 
+VALUES ( 'Gert' , 'Gertzoon' , 1998-03-08, 'Uithoorn');
 
-INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats)
-VALUES (9, 'Janus' , 'Pipelanus' , '13', '1994-04-25', 'Uithoorn')
+INSERT INTO studenten ( voornaam , achternaam, geboortedatum,woonplaats ) 
+VALUES ( 'Piet' , 'Pietzoon' , 1998-02-08, 'Uithoorn');
 
-INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats) 
-VALUES (10, 'Esmeralda' , 'Alvarez' , '13', '1998-06-26', 'Uithoorn')
+INSERT INTO studenten ( voornaam , achternaam, geboortedatum,woonplaats ) 
+VALUES ( 'Dirk' , 'Dirkzoon' , 1998-05-08, 'Uithoorn');
