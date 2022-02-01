fe2e295bbd0860524743c0b877f3087a064a081c
Thomas Mooij <2021747@talnet.nl>
2022-02-01 14:23:01 +0100

    update 1 af

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,7 +1,18 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats='Amstelveen'
 -- Opdracht 2 
+UPDATE studenten2
+SET adres="Uithoornstraat", postcode="1433kk"
+WHERE id="8"
 
+UPDATE studenten2
+SET adres="Uithoornerstraat", postcode="1433ak"
+WHERE id="9"
+
+UPDATE studenten2
+SET adres="Uithoornsestraat", postcode="1433bk"
+WHERE id="10"
 -- Opdracht 3
 
 -- Opdracht 4
