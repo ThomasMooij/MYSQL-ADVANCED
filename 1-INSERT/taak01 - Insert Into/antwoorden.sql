8be7be9edb72ddad0732264999cc2ad0c7cc56a4
Thomas Mooij <2021747@talnet.nl>
2022-02-17 11:51:56 +0100

    insert into studenten toegeveogd

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -4,3 +4,15 @@ VALUES (1, 'Gert', 'Gertus', '13A', 'Mijnstraat11', '1234AB', '1910-06-18', 'Uit
 
 INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
 VALUES (2, 'Piet', 'Pieters', '13A', 'straatstraat13', '1234AB', '1910-04-07', 'Uithoorn');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (2, 'Pietz', 'Pieterz', '1A', 'straatstraat16', '1234AB', '1910-04-07', 'Uithoorn');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (2, 'Pieter', 'Piets', '13b', 'straattraat13', '1234AB', '1910-04-07', 'Uithoorn');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (2, 'Pit', 'Pit', '13A', 'straatstraat13', '1234AB', '1910-04-07', 'Uithoorn');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (2, 'jan', 'jaap', '13A', 'straatstraat13', '1234AB', '1910-04-07', 'Uithoorn');
